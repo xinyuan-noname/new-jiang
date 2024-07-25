5387623acb68f16e65b74070217201537299f73b
Your Name <13257656+xinyuanwm@user.noreply.gitee.com~>
2024-07-25 15:49:33 +0800

    引入node来帮助管理扩展

--- Directory.js
+++ Directory.js
@@ -1,182 +1,178 @@
 window["xjb_xyAPI_Directory_新将包"]={
     "main": [
-        "xjbhan_xunyu.jpg",
-        "xjbhan_caocao.jpg",
-        "xjb_zhouyu.jpg",
-        "xjb_zhihuan.png",
-        "xjb_zhugeliang.jpg",
-        "xjb_xyAPI.js",
-        "xjb_yingzheng.jpg",
-        "xjb_zhangliang_liuhou.jpg",
-        "xjb_sunce.jpg",
-        "xjb_tianming_huobi2.png",
-        "xjb_tianming_huobi1.png",
-        "xjb_seizeHpCard.png",
-        "xjb_pangtong.jpg",
-        "xjb_shenshapo.png",
-        "xjb_machao.jpg",
-        "xjb_liushan.jpg",
-        "xjb_jingu.png",
-        "xjb_huatuo.jpg",
-        "xjb_jiaxu.jpg",
-        "xin_qixing.png",
+        ".gitignore",
+        "card_lw.jpg",
         "Directory.js",
-        "xjb_ganning.jpg",
-        "xjb_guanyu.jpg",
-        "xjb_guojia.jpg",
-        "xjb_daqiao.jpg",
-        "xjb_dianwei.jpg",
-        "xjb_fazheng.jpg",
-        "xjb_Infinity.png",
-        "xin_qinnangshu.png",
-        "xjb_caocao.jpg",
+        "extension.js",
+        "info.json",
+        "LICENSE",
+        "qimendunjia.png",
+        "README.md",
+        "skillCard.png",
+        "soul_chanter.jpg",
+        "soul_Fuaipaiyi.jpg",
+        "soul_timer.jpg",
+        "xin_baiyin.png",
+        "xin_chitu.png",
+        "xin_fellow.jpg",
         "xin_hutou.png",
         "xin_newCharacter.jpg",
         "xin_qinglong.png",
-        "xin_baiyin.png",
-        "xin_fellow.jpg",
-        "xin_chitu.png",
-        "LICENSE",
-        "skillCard.png",
-        "soul_Fuaipaiyi.jpg",
-        "soul_timer.jpg",
-        "soul_chanter.jpg",
-        "extension.js",
-        ".gitignore",
-        "card_lw.jpg",
-        "qimendunjia.png",
-        "info.json",
-        "xjb_jin_simayi.jpg"
-    ],
-    "title": [
-        "xjb_kill2.png",
-        "xjb_yin3.png",
-        "xjb_ice.png",
-        "xjb_boss1.png",
-        "xjb_gainMaxHp.png",
-        "xjb_damageZero.png",
-        "xjb_loseMaxHp.png",
-        "xjb_yin1.png",
-        "xjb_thunder.png",
-        "xjb_yin2.png",
-        "xjb_fire.png",
-        "xjb_kill1.png"
-    ],
-    "text": [
-        "lingliData.txt"
-    ],
-    "position": [
-        "Thumbs.db",
-        "redSpace.jpg",
-        "azureSky.jpg",
-        "lake.jpg",
-        "library.jpg"
-    ],
-    "log": [
-        "log.txt"
-    ],
-    "lingli": [
-        "tear.jpg",
-        "book.jpg",
-        "blood.jpg",
-        "dragon.jpg",
-        "Thumbs.db",
-        "timer.jpg",
-        "gathering.jpg",
-        "check.png",
-        "taoyao.jpg",
-        "sun.jpg",
-        "flower.jpg",
-        "xuemo.jpg"
-    ],
-    "js": [
-        "skills.js",
-        "event.js",
-        "math.js",
-        "rpg.js",
-        "title.js",
-        "lingli.js",
-        "translate.js",
-        "economy.js",
-        "project.js",
-        "canvas.js",
-        "editor.js",
-        "final.js",
-        "library.js",
-        "Xskill.js",
-        "dialog.js",
-        "card.js",
-        "string.js",
-        "ui.js",
-        "nonameCN.js"
-    ],
-    "image": [
-        "Lucky.png",
-        "edit.png",
-        "god.jpg",
-        "xjb_shop.png",
-        "tool.png",
-        "xjb_locked.png",
-        "π.png",
-        "instruction.png",
-        "trash.png",
-        "xjb_close.png",
-        "zeroize.png",
-        "Thumbs.db",
-        "xjb_hunbi.png",
-        "xjb_open.png"
-    ],
-    "hpCard": [
-        "4.jpg",
-        "1.jpg",
-        "2.jpg",
-        "Thumbs.db",
-        "3.jpg",
-        "5.jpg"
-    ],
-    "css": [
-        "css1.css"
+        "xin_qinnangshu.png",
+        "xin_qixing.png",
+        "xjbhan_caocao.jpg",
+        "xjbhan_xunyu.jpg",
+        "xjb_caocao.jpg",
+        "xjb_daqiao.jpg",
+        "xjb_dianwei.jpg",
+        "xjb_fazheng.jpg",
+        "xjb_ganning.jpg",
+        "xjb_guanyu.jpg",
+        "xjb_guojia.jpg",
+        "xjb_huatuo.jpg",
+        "xjb_Infinity.png",
+        "xjb_jiaxu.jpg",
+        "xjb_jingu.png",
+        "xjb_jin_simayi.jpg",
+        "xjb_liushan.jpg",
+        "xjb_machao.jpg",
+        "xjb_pangtong.jpg",
+        "xjb_seizeHpCard.png",
+        "xjb_shenshapo.png",
+        "xjb_sunce.jpg",
+        "xjb_tianming_huobi1.png",
+        "xjb_tianming_huobi2.png",
+        "xjb_xyAPI.js",
+        "xjb_yingzheng.jpg",
+        "xjb_zhangliang_liuhou.jpg",
+        "xjb_zhihuan.png",
+        "xjb_zhouyu.jpg",
+        "xjb_zhugeliang.jpg"
     ],
     "audio": [
         "fire1.mp3"
     ],
-    "sink": [],
+    "css": [
+        "css1.css"
+    ],
+    "hpCard": [
+        "1.jpg",
+        "2.jpg",
+        "3.jpg",
+        "4.jpg",
+        "5.jpg"
+    ],
+    "image": [
+        "edit.png",
+        "god.jpg",
+        "instruction.png",
+        "Lucky.png",
+        "tool.png",
+        "trash.png",
+        "xjb_close.png",
+        "xjb_hunbi.png",
+        "xjb_locked.png",
+        "xjb_open.png",
+        "xjb_shop.png",
+        "zeroize.png",
+        "π.png"
+    ],
+    "js": [
+        "canvas.js",
+        "card.js",
+        "dialog.js",
+        "economy.js",
+        "editor.js",
+        "event.js",
+        "final.js",
+        "library.js",
+        "lingli.js",
+        "math.js",
+        "nonameCN.js",
+        "project.js",
+        "rpg.js",
+        "skills.js",
+        "string.js",
+        "title.js",
+        "translate.js",
+        "ui.js",
+        "Xskill.js"
+    ],
     "json": [
         "1.json"
     ],
-    ".vscode": [
-        "extensions.json",
-        "settings.json"
+    "lingli": [
+        "blood.jpg",
+        "book.jpg",
+        "check.png",
+        "dragon.jpg",
+        "flower.jpg",
+        "gathering.jpg",
+        "sun.jpg",
+        "taoyao.jpg",
+        "tear.jpg",
+        "timer.jpg",
+        "xuemo.jpg"
+    ],
+    "log": [
+        "log.txt"
     ],
     "md": [
         "skillEditor.md"
     ],
-    "image/card": [
-        "xjb_xiugengxuzi.png",
+    "node": [
+        "gen_dir.js"
+    ],
+    "other": [],
+    "position": [
+        "azureSky.jpg",
+        "lake.jpg",
+        "library.jpg",
+        "redSpace.jpg"
+    ],
+    "sink": [],
+    "text": [
+        "lingliData.txt"
+    ],
+    "title": [
+        "xjb_boss1.png",
+        "xjb_damageZero.png",
+        "xjb_fire.png",
+        "xjb_gainMaxHp.png",
+        "xjb_ice.png",
+        "xjb_kill1.png",
+        "xjb_kill2.png",
+        "xjb_loseMaxHp.png",
+        "xjb_thunder.png",
+        "xjb_yin1.png",
+        "xjb_yin2.png",
+        "xjb_yin3.png"
+    ],
+    "image\\card": [
+        "xjb_chucanquhui.png",
         "xjb_lijingtuzhi.png",
-        "xjb_chucanquhui.png"
+        "xjb_xiugengxuzi.png"
     ],
-    "sink/Red": [
-        "红-鬼.png",
-        "小红2.png",
-        "红-白.png",
-        "红2.png",
-        "红-祸.png",
-        "红3.png",
-        "红1.png",
-        "小红3.png",
-        "Thumbs.db",
+    "sink\\Red": [
+        "小红.png",
         "小红1.png",
-        "小红.png"
+        "小红2.png",
+        "小红3.png",
+        "红-白.png",
+        "红-祸.png",
+        "红-鬼.png",
+        "红1.png",
+        "红2.png",
+        "红3.png"
     ],
-    "sink/xjb_xuemo": [
-        "xuemo2.jpg",
+    "sink\\xin_newCharacter": [],
+    "sink\\xjb_xuemo": [
         "xuemo1.jpg",
+        "xuemo2.jpg",
         "xuemo3.jpg",
         "xuemo4.jpg"
     ],
-    "sink/xin_newCharacter": [],
-    "sink/xin_newCharacter/normal": [
+    "sink\\xin_newCharacter\\normal": [
         "xin_newCharacter.jpg"
     ]
-} 
+}
---
+++ node/gen_dir.js
@@ -0,0 +1,117 @@
+const fs = require('fs').promises
+const pathModule = require('path')
+class fsPromise {
+    static async readDir(path, log = true) {
+        const main = await fs.readdir(path)
+        log && console.log(main)
+        return main;
+    }
+    static async classifyFileDir(path, log = true) {
+        const all = await this.readDir(path, false);
+        const _file = [];
+        const _dir = [];
+        for (const fileName of all) {
+            const entry = await fs.stat(pathModule.join(path, fileName))
+            entry.isFile() && _file.push(fileName);
+            entry.isDirectory() && _dir.push(fileName);
+        }
+        const result = { _file, _dir }
+        log && console.log(result)
+        return result;
+    }
+    static async showDir(path, log = true) {
+        const map = await this.classifyFileDir(path, false);
+        map.main = map._file;
+        delete map._file;
+        for (let i = 0; i < 10; i++) {
+            map.directories = []
+            for (const dirName of map._dir) {
+                map[dirName] = [];
+                const next = await this.classifyFileDir(
+                    pathModule.join(path, dirName),
+                    false
+                );
+                map[dirName] = [...next._file]
+                map.directories = map.directories.concat(next._dir.map(each => {
+                    return pathModule.join(dirName, each)
+                }))
+            }
+            map._dir = [...map.directories];
+        }
+        delete map._dir;
+        delete map.directories;
+        log && console.log(map);
+        return map;
+    }
+    static async showPathes(path, log = true) {
+        const map = await this.showDir(path, false);
+        const result = [];
+        map.main.forEach(file => {
+            result.push(
+                pathModule.join(path, file)
+            )
+        })
+        delete map.main;
+        for (const [dirName, files] of Object.entries(map)) {
+            files.forEach(file => {
+                result.push(
+                    pathModule.join(path, dirName, file)
+                )
+            })
+        }
+        log && console.log(result);
+        return result;
+    }
+    /**
+     * 
+     * @param {string} path 
+     * @param {function} filterDir 
+     * @param {function} filterFile 
+     * @param {boolean} log 
+     * @returns {object}
+     */
+    static async showDirFilter(path, filterDir, filterFile, log = true) {
+        const map = await this.showDir(path, false);
+        const result = {}
+        for (const [dirName, files] of Object.entries(map)) {
+            if (!filterDir(dirName)) continue;
+            result[dirName] = files.filter(filterFile);
+        }
+        log && console.log(result)
+        return result;
+    }
+    static async generateText(path, content, sourcePath) {
+        if (content == 'pathes' && sourcePath) {
+            content = (await this.showPathes(sourcePath, false)).join('\n')
+        }
+        await fs.writeFile(path, content)
+    }
+    static async clearContent(path) {
+        await fs.writeFile(path, '')
+    }
+    static async unlinkFiles(path, judge) {
+        const map = await this.showPathes(path, false)
+        map.forEach(file => {
+            judge(file) && fs.unlink(file)
+        })
+    }
+
+}
+fsPromise.showDirFilter(
+    '../',
+    dirName => {
+        return !dirName.startsWith('.git') && !dirName.startsWith('.vscode')
+    },
+    (file, index) => {
+        if (file === 'Thumbs.db') return false;
+        return true;
+    }
+).then(data => {
+    fs.writeFile(
+        '../Directory.js',
+        `window["xjb_xyAPI_Directory_新将包"]=${JSON.stringify(data, null, 4)}`
+    )
+})
+fsPromise.unlinkFiles('../', file => file.includes('Thumbs.db'))
+fsPromise.clearContent('../json/1.json')
+fsPromise.clearContent('../log/log.txt')
